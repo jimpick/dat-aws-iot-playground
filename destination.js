518be3d5f6f4f4f7d858e3b157cada196f48a557
Jim Pick <jim@backblend.com>
2017-12-28 09:39:52 -0800

    Add code to create sequence numbers and resequence messages
    Amazon's IoT MQTT implementation delivers messages in random order

--- destination.js
+++ destination.js
@@ -1,22 +1,212 @@
+const { Duplex } = require('stream')
 const Dat = require('dat-node')
-const wss = require('websocket-stream')
 const pump = require('pump')
+const through2 = require('through2')
 const port = process.env.PORT || 50000
 
+const awsIot = require('aws-iot-device-sdk');
+
+const AWS = require('aws-sdk');
+AWS.config.region = 'us-east-1';
+
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-east-1:7aadae5c-7106-4616-b5a1-1e24ed4a8b93',
+});
+const iotEndpoint = 'a1i7z9mmi9v21.iot.us-east-1.amazonaws.com';
 const key = '96ab03a6aeca21f53182d78ad9672f0313ee26dd10462d1ab9c27c48d8d7a82d'
 
-Dat('./data-destination', {key}, function (err, dat) {
-  if (err) throw err
+let client, iotTopic;
 
-  console.log(`dat://${dat.key.toString('hex')}`)
+const IoT = {
+  connect: (topic, iotEndpoint, region, accessKey, secretKey, sessionToken) => {
 
-  const stream = wss(`ws://localhost:${port}`)
-  pump(
-    stream,
-    dat.archive.replicate({live: true, encrypt: false}),
-    stream
-  )
-  dat.archive.on('update', () => {
-    console.log('update', dat.archive.version)
+    iotTopic = topic;
+
+    client = awsIot.device({
+      region: region,
+      protocol: 'wss',
+      accessKeyId: accessKey,
+      secretKey: secretKey,
+      sessionToken: sessionToken,
+      port: 443,
+      host: iotEndpoint
+    });
+
+    client.on('connect', onConnect);
+    // client.on('message', onMessage);
+    client.on('error', onError);
+    client.on('reconnect', onReconnect);
+    client.on('offline', onOffline);
+    client.on('close', onClose);
+  },
+
+  send: (message) => {
+      client.publish(iotTopic, message);
+  }
+};
+
+const onConnect = () => {
+    client.subscribe(iotTopic);
+    console.log('Connected');
+};
+
+const onMessage = (topic, message) => {
+    console.log(message.toString());
+};
+
+const onError = () => {};
+const onReconnect = () => {};
+const onOffline = () => {};
+
+const onClose = () => {
+    console.log('Connection failed');
+};
+
+class IoTStream extends Duplex {
+  constructor (options) {
+    super(options)
+    // Should probably not use globals
+    this.buffering = true
+    this.readBuffer = {}
+    this.pushedSeq = 0
+    this.maxQueuedSeq = 0
+  }
+
+  _write (chunk, encoding, cb) {
+    const message = chunk
+    console.log('Jim sent', typeof message, message.length, message)
+    /*
+    client.publish('/serverless/from-dest', message, err => {
+      if (err) {
+        console.log('Jim cb', err)
+        cb(err)
+      {
+      cb()
+    })
+    */
+    cb()
+  }
+
+  _read (size) {
+    // Do nothing. Use .push() instead
+    console.log('Jim _read', size, Object.keys(this.readBuffer).length)
+    this.buffering = false
+    this.processQueue()
+  }
+
+  ingest (seqMessage) {
+    const seq = seqMessage.readUInt32LE(0) // Is it always LE?
+    const message = seqMessage.slice(4)
+    console.log('Jim queued', seq, message)
+    this.readBuffer[seq] = message
+    this.maxQueuedSeq = Math.max(this.maxQueuedSeq, seq)
+    this.processQueue()
+  }
+
+  processQueue () {
+    if (this.buffering) return
+    while (this.pushedSeq < this.maxQueuedSeq) {
+      const nextSeq = this.pushedSeq + 1
+      const message = this.readBuffer[nextSeq]
+      if (!message) return
+      const pushed = this.push(message)
+      delete this.readBuffer[nextSeq]
+      this.pushedSeq = nextSeq
+      console.log('Jim pushed', nextSeq, this.maxQueuedSeq, pushed,
+        this._readableState.flowing)
+      if (!pushed) {
+        this.buffering = true
+        return
+      }
+    }
+  }
+}
+
+const oldEnd = IoTStream.prototype.end
+IoTStream.prototype.end = function () {
+  // console.trace('Jim end', oldEnd)
+  console.log('Jim end')
+  oldEnd.apply(this, Array.from(arguments))
+}
+
+function run () {
+  let iotKeys;
+
+  AWS.config.credentials.get(() => {
+    // Use these credentials with IoT
+    const accessKey = AWS.config.credentials.accessKeyId;
+    const secretKey = AWS.config.credentials.secretAccessKey;
+    const sessionToken = AWS.config.credentials.sessionToken;
+    iotKeys = {
+        iotEndpoint,
+        region: AWS.config.region,
+        accessKey,
+        secretKey,
+        sessionToken
+    }
+    console.log(`Endpoint: ${iotKeys.iotEndpoint},
+            Region: ${iotKeys.region},
+            AccessKey: ${iotKeys.accessKey},
+            SecretKey: ${iotKeys.secretKey},
+            SessionToken: ${iotKeys.sessionToken}`);
+
+    const iotTopic = '/serverless/#';
+
+    IoT.connect(iotTopic,
+                iotKeys.iotEndpoint,
+                iotKeys.region,
+                iotKeys.accessKey,
+                iotKeys.secretKey,
+                iotKeys.sessionToken);
+
+    Dat('./data-destination', {key}, function (err, dat) {
+      if (err) throw err
+
+      console.log(`dat://${dat.key.toString('hex')}`)
+
+      // const stream = new IoTStream({ objectMode: true })
+      const stream = new IoTStream()
+      client.on('message', (topic, seqMessage) => {
+        if (topic !== '/serverless/from-src') return
+        console.log('Jim received', topic, typeof seqMessage,
+          seqMessage.length, seqMessage)
+        stream.ingest(seqMessage)
+      })
+      stream.on('end', () => console.log('end'))
+      stream.on('finish', () => console.log('finish'))
+      stream.on('error', err => console.log('error', err))
+      // console.log('Press a key')
+      // process.stdin.once('data', function () {
+      setTimeout(() => {
+        console.log('Pump')
+        /*
+        const replicate = dat.archive.replicate({live: true, encrypt: false})
+        replicate.on('end', () => console.log('replicate end'))
+        replicate.on('finish', () => console.log('replicate finish'))
+        replicate.on('error', err => console.log('replicate error', err))
+        */
+        pump(
+          stream,
+					through2(function (chunk, enc, cb) {
+						console.log('From src', typeof chunk, chunk.length, chunk)
+						this.push(chunk)
+						cb()
+					})
+          /* replicate,
+					through2(function (chunk, enc, cb) {
+						console.log('From dest', typeof chunk, chunk.length, chunk)
+						this.push(chunk)
+						cb()
+					}),
+          stream
+          */
+        )
+        dat.archive.on('update', () => {
+          console.log('update', dat.archive.version)
+        })
+      }, 5000)
+    })
   })
-})
+}
+
+run()
--- source.js
+++ source.js
@@ -1,6 +1,7 @@
 const { Duplex } = require('stream')
 const Dat = require('dat-node')
 const pump = require('pump')
+const through2 = require('through2')
 const port = process.env.PORT || 50000
 
 const awsIot = require('aws-iot-device-sdk');
@@ -14,33 +15,37 @@ AWS.config.credentials = new AWS.CognitoIdentityCredentials({
 const iotEndpoint = 'a1i7z9mmi9v21.iot.us-east-1.amazonaws.com';
 
 let client, iotTopic;
+let buffering = true
+const readBuffer = []
+
 const IoT = {
+  connect: (topic, iotEndpoint, region, accessKey, secretKey, sessionToken) => {
 
-    connect: (topic, iotEndpoint, region, accessKey, secretKey, sessionToken) => {
+    iotTopic = topic;
 
-        iotTopic = topic;
+    client = awsIot.device({
+      region: region,
+      protocol: 'wss',
+      accessKeyId: accessKey,
+      secretKey: secretKey,
+      sessionToken: sessionToken,
+      port: 443,
+      host: iotEndpoint
+    });
 
-        client = awsIot.device({
-            region: region,
-            protocol: 'wss',
-            accessKeyId: accessKey,
-            secretKey: secretKey,
-            sessionToken: sessionToken,
-            port: 443,
-            host: iotEndpoint
-        });
+    client.on('connect', onConnect);
+    // client.on('message', onMessage);
+    client.on('error', onError);
+    client.on('reconnect', onReconnect);
+    client.on('offline', onOffline);
+    client.on('close', onClose);
+  },
 
-        client.on('connect', onConnect);
-        // client.on('message', onMessage);
-        client.on('error', onError);
-        client.on('reconnect', onReconnect);
-        client.on('offline', onOffline);
-        client.on('close', onClose);
-    },
-
-    send: (message) => {
-        client.publish(iotTopic, message);
-    }
+  /*
+  send: (message) => {
+      client.publish(iotTopic, message);
+  }
+  */
 };
 
 const onConnect = () => {
@@ -63,16 +68,27 @@ const onClose = () => {
 class IoTStream extends Duplex {
   constructor () {
     super()
+    this.sequence = new Uint32Array(1)
+    this.sequence[0] = 0
     // Should probably not use globals
   }
 
   _write (chunk, encoding, cb) {
     const message = chunk
-    console.log('Jim sent', typeof message)
-    client.publish(iotTopic, message)
+    console.log('Jim sent', this.sequence[0], typeof message,
+      message.length, message)
+    this.sequence[0]++;
+    const seqMessage = Buffer.concat([
+      Buffer.from(this.sequence.buffer),
+      message
+    ])
+    console.log('Jim sent2', seqMessage)
+    client.publish('/serverless/from-src', seqMessage, cb)
   }
 
   _read (size) {
+    console.log('Jim _read', size, readBuffer.length)
+    buffering = false
     // Do nothing. Use .push() instead
   }
 }
@@ -82,51 +98,79 @@ function run () {
   let iotKeys;
 
   AWS.config.credentials.get(() => {
-      // Use these credentials with IoT
-      const accessKey = AWS.config.credentials.accessKeyId;
-      const secretKey = AWS.config.credentials.secretAccessKey;
-      const sessionToken = AWS.config.credentials.sessionToken;
-      iotKeys = {
-          iotEndpoint,
-          region: AWS.config.region,
-          accessKey,
-          secretKey,
-          sessionToken
-      }
-      console.log(`Endpoint: ${iotKeys.iotEndpoint},
-              Region: ${iotKeys.region},
-              AccessKey: ${iotKeys.accessKey},
-              SecretKey: ${iotKeys.secretKey},
-              SessionToken: ${iotKeys.sessionToken}`);
+    // Use these credentials with IoT
+    const accessKey = AWS.config.credentials.accessKeyId;
+    const secretKey = AWS.config.credentials.secretAccessKey;
+    const sessionToken = AWS.config.credentials.sessionToken;
+    iotKeys = {
+        iotEndpoint,
+        region: AWS.config.region,
+        accessKey,
+        secretKey,
+        sessionToken
+    }
+    console.log(`Endpoint: ${iotKeys.iotEndpoint},
+            Region: ${iotKeys.region},
+            AccessKey: ${iotKeys.accessKey},
+            SecretKey: ${iotKeys.secretKey},
+            SessionToken: ${iotKeys.sessionToken}`);
 
-      const iotTopic = '/serverless/pubsub';
+    const iotTopic = '/serverless/#';
 
-      IoT.connect(iotTopic,
-                  iotKeys.iotEndpoint,
-                  iotKeys.region,
-                  iotKeys.accessKey,
-                  iotKeys.secretKey,
-                  iotKeys.sessionToken);
+    IoT.connect(iotTopic,
+                iotKeys.iotEndpoint,
+                iotKeys.region,
+                iotKeys.accessKey,
+                iotKeys.secretKey,
+                iotKeys.sessionToken);
 
-      // IoT.send(msg);
-      Dat('./data-source', function (err, dat) {
-        if (err) throw err
+    // IoT.send(msg);
+    Dat('./data-source', function (err, dat) {
+      if (err) throw err
 
-        dat.importFiles()
+      dat.importFiles()
 
-        console.log(`dat://${dat.key.toString('hex')}`)
+      console.log(`dat://${dat.key.toString('hex')}`)
 
-        const stream = new IoTStream()
-        client.on('message', (topic, message) => {
-          console.log('Jim received', typeof message)
-          stream.push(message)
-        })
+      const stream = new IoTStream()
+      client.on('message', (topic, seqMessage) => {
+        if (topic !== '/serverless/from-dest') return
+        console.log('Jim received', topic, typeof seqMessage,
+          seqMessage.length, seqMessage)
+        if (!buffering) {
+          /*
+          const pushed = stream.push(seqMessage)
+          // stream.resume()
+          console.log('Jim pushed', pushed, stream._readableState.flowing)
+          if (!pushed) {
+            buffering = true
+          }
+          */
+        } else {
+          readBuffer.push(seqMessage)
+        }
+      })
+      // console.log('Press a key')
+      // process.stdin.once('data', function () {
+      setTimeout(() => {
+        console.log('Pump')
         pump(
           stream,
+					through2(function (chunk, enc, cb) {
+						console.log('From dest', typeof chunk, chunk.length, chunk)
+						this.push(chunk)
+						cb()
+					}),
           dat.archive.replicate({live: true, encrypt: false}),
+					through2(function (chunk, enc, cb) {
+						console.log('From src', typeof chunk, chunk.length, chunk)
+						this.push(chunk)
+						cb()
+					}),
           stream
         )
-      })
+      }, 5000)
+    })
 
   });
 
